3b0d81d9829386366f6c67e9ff5d15081022bb3f
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-16 19:03:01 -0500

    day1complete

---
+++ day1/band_name_generator.py
@@ -0,0 +1,7 @@
+print("Greetings Minions, Are you ready to rock! Let's figure out your band name.\n")
+
+city = input("What city did you grow up in?\n")
+
+pet = input("What is your pets name?\n")
+
+print(city +" "+ pet)
---
+++ day1/main.py
@@ -0,0 +1,7 @@
+a = input("What is your first name?")
+b = input("What is your last name?")
+c=a
+b=a
+c=b
+print(a)
+print(b)
