8e131da0e6833583d7f63e4ab9e32bf8f1252593
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-18 20:24:24 -0500

    datatypes

---
+++ day2/main.py
@@ -0,0 +1,7 @@
+two_digit_number = input("type a two digit number")
+new_two_digit_number = str(two_digit_number)
+a = int(new_two_digit_number[0])
+b = int(new_two_digit_number[1])
+print(a + b)
+
+
