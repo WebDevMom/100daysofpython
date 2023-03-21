77d058fcda61d8e57eeaed2379a626bed2fdf91b
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-20 22:55:48 -0500

    finished life in weeks project

--- day2/life_in_weeks.py
+++ day2/life_in_weeks.py
@@ -1,1 +1,7 @@
-a
+age = input("What is your current age? ")
+life_span = 90
+time_left = life_span - int(age)
+y = time_left * 52
+z = time_left * 12
+x = time_left * 365
+print(f"You have {x} days, {y} weeks, and {z} months left")
