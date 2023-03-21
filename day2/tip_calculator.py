77a8dd41192ee66a09f734ad5450c9003efa2a57
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-21 16:57:19 -0500

    added notes to tip_calculator

--- day2/tip_calculator.py
+++ day2/tip_calculator.py
@@ -1,1 +1,8 @@
-#tip calculator
+#tip calculator-#bill/num of people*1.12
+
+print("Welcome to the tip calculator. ")
+input("What was the total bill?")
+input("What percentage tip would you like to give? 10, 12, or 15?")
+input("How many people split the bill?")
+
+print("Each person shuld pay: result")
