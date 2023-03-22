deac9b4dba269686a156d7b20d2df6e8da14ce9c
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-21 19:06:33 -0500

    finsihed tip calculator project

--- day2/tip_calculator.py
+++ day2/tip_calculator.py
@@ -1,8 +1,13 @@
 #tip calculator-#bill/num of people*1.12
 
 print("Welcome to the tip calculator. ")
-input("What was the total bill?")
-input("What percentage tip would you like to give? 10, 12, or 15?")
-input("How many people split the bill?")
+total_bill = float(input(f"What was the total bill? $"))
 
-print("Each person shuld pay: result")
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15?"))
+
+customers = int(input("How many people split the bill?"))
+
+result = total_bill/customers * (tip/100+1)
+new_result = round(result, 2)
+print(f"Each person should pay: {new_result}")
+
