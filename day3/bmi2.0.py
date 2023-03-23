a07049c329bbaad9da284bd53162187703866e85
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-22 22:45:48 -0500

    updated code

--- day3/bmi2.0.py
+++ day3/bmi2.0.py
@@ -1,2 +1,17 @@
 height = float(input("enter your height in m: "))
 weight = float(input("enter your weight in kg: "))
+a = float(height)* float(height)
+b = int(weight)
+print(int(b/a))
+
+
+
+
+
+
+
+#Under 18.5 they are underweight
+#Over 18.5 but below 25 they have a normal weight
+#Over 25 but below 30 they are slightly overweight
+#Over 30 but below 35 they are obese
+#Above 35 they are clinically obese.
