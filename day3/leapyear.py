170f257cfe6f8a4dd5b058fc37961ca4d8f7af90
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-24 21:15:19 -0500

    added ifelif to program

--- day3/leapyear.py
+++ day3/leapyear.py
@@ -1,1 +1,22 @@
 #leap year
+year = int(input("Which year do you want to check?"))
+if year % 4 == 0:
+    print("leap year.")
+elif year % 100 == 0:
+    print("leap year.")
+elif year % 400 == 0:
+    print("leap year.")
+
+
+
+ 
+
+
+
+#for every year that is evenly divible by 4 
+#except every year that is evenly divisible by 100
+#unless the year is also evenly divible by 400
+#ex. year 2000
+#2000/4 = 500 -leap
+#2000/100 = 20 not leap
+#2000/400 = 5 leap
