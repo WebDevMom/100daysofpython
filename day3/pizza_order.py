befe6b93f6e9920a0c7caf259daf3e6123f53761
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-25 23:57:00 -0500

    created pizza_order.py

---
+++ day3/pizza_order.py
@@ -0,0 +1,25 @@
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+bill = 0
+S = 15
+M = 20
+L = 25
+add_pepp_small = 2
+add_pepp_med_lg = 3
+add_extra_cheeese = 1
+
+if size = 
+if add_pepp_small
+if add_pepp_med_lg
+if add_extra_cheeese
+
+
+
+#Small Pizza: $15
+#Medium Pizza: $20
+#Large Pizza: $25
+#Pepperoni for Small Pizza: +$2
+#Pepperoni for Medium or Large Pizza: +$3
+#Extra cheese for any size pizza: + $1
