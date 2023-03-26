f4e18aa97a85863b18abbef95e66ec6deee59cfc
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-26 00:37:38 -0500

    completed pizza order program

--- day3/pizza_order.py
+++ day3/pizza_order.py
@@ -3,21 +3,30 @@ size = input("What size pizza do you want? S, M, or L ")
 add_pepperoni = input("Do you want pepperoni? Y or N ")
 extra_cheese = input("Do you want extra cheese? Y or N ")
 bill = 0
-S = 15
-M = 20
-L = 25
-add_pepp_small = 2
-add_pepp_med_lg = 3
-add_extra_cheeese = 1
+small_pizza_cost = 15
+med_pizza_cost = 20
+large_pizza_cost = 25
+add_pepp_small_cost = 2
+add_pepp_med_lg_cost = 3
+add_extra_cheeese_cost = 1
 
-if size ==
-if add_pepp_small =
-if add_pepp_med_lg =
-if add_extra_cheeese =
+if size == "S":
+    bill += small_pizza_cost
+    if add_pepperoni == "Y":
+        bill += add_pepp_small_cost
+elif size == "M":
+    bill += med_pizza_cost
+    if add_pepperoni == "Y":
+        bill += add_pepp_med_lg_cost
+elif size == "L":
+    bill += large_pizza_cost
+    if add_pepperoni == "Y":
+        bill += add_pepp_med_lg_cost
+        
+if extra_cheese == "Y":
+    bill += add_extra_cheeese_cost
 
-
-
-print(f"Your final bill is ${bill}")
+print(f"Your final bill is ${bill}.")
 
 #Small Pizza: $15
 #Medium Pizza: $20
