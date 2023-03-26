5639fe2ca7eb65d014c9fd0be45fc2395d0870d9
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-25 23:11:31 -0500

    corrected punctuation

--- day3/leapyear.py
+++ day3/leapyear.py
@@ -1,13 +1,13 @@
 #leap year
 year = int(input("Which year do you want to check?"))
 if year % 4 == 0:
-    print("leap year.")
+    print("Leap year.")
 elif year % 100 == 0:
-    print("not leap year.")
+    print("Not leap year.")
 elif year % 400 == 0:
-    print("leap year.")
+    print("Leap year.")
 else:
-    print("not leap year")
+    print("Not leap year.")
 
 
 
