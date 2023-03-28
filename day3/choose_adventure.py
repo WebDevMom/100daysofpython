003351024c51e0d9dfd5c00b570829fd4c143084
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-27 21:19:23 -0500

    added graphic and more story

--- day3/choose_adventure.py
+++ day3/choose_adventure.py
@@ -17,12 +17,12 @@ print('''
 print("Welcome to Space.")
 print("Your mission is to explore space and find the alien life.") 
 print("Your ship approaches a small moon!")
-moon_land = input("Do you land on the moon and explore? Y or N?\n")
-if moon_land == "Y":
+moon_land = input("Do you land on the moon and explore? Y or N?\n").lower()
+if moon_land == "y":
     print("You exit the ship and are immediately abliterated by aliens. You are dead! GAME OVER")
-elif moon_land == "N":
+elif moon_land == "n":
     print("You travel further into space searhing for aliens!")
-print('''
+    print('''
  *********************************************************************************************
                    |||
                   +++++
@@ -35,10 +35,30 @@ print('''
 
 ***************************************************************************************
 ''')
-print("You travel to a distant star and hear transmissions coming from the surface.")
-distant_star = input("Do you approach and make contact? Y or N?\n")
-if distant_star == "Y":
-    print("Lets make contact!")
-elif distant_star == "N":
-    print("The civilization on the star takes your lack of communication as a threat and destroys your ship.Your dead! GAME OVER.")
-print("You reach and decide to land on the planet. You have discovered a friendly alien race. You win the game!")
+    print("You travel to a distant star and hear transmissions coming from the surface.")
+    distant_star = input("Do you approach and make contact? Y or N?\n").lower()
+    if distant_star == "y":
+        print("Lets make contact! You decide to land on the planet. You have discovered a friendly alien race.")
+    elif distant_star == "n":
+        print("The civilization on the star takes your lack of communication as a threat and destroys your ship.Your dead! GAME OVER.")
+        print('''
+*******************************************************************************
+            .-""""-.        .-""""-.
+           /        \      /        \
+          /_        _\    /_        _\
+         // \      / \\  // \      / \\
+         |\__\    /__/|  |\__\    /__/|
+          \    ||    /    \    ||    /
+           \        /      \        /
+            \  ^^  /        \  __  /
+             '.__.'          '.__.'
+              |  |            |  |
+              |  |            |  |
+*****************************************************************************
+''')
+    print("You meet a beatiful alien female on the star.")
+    home = input("Do you stay on the star and make it your home? stay or go?\n")
+    if home == "stay":
+            print("You go native and live happily ever after with your alien bride.")
+    if home == "go":
+            print("You leave the star and return to earth to a meagar existance.")
