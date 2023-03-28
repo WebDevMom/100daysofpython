d939dd3431a448e933deb0ea41120ce5fdeadf5a
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-27 20:13:30 -0500

    added story and more images

--- day3/choose_adventure.py
+++ day3/choose_adventure.py
@@ -17,4 +17,22 @@ print('''
 print("Welcome to Space.")
 print("Your mission is to find the alien life.") 
 print("Your ship approaches a small moon!")
-input("Do you land on the moon and explore? Y or N")
+moon_land = input("Do you land on the moon and explore? Y or N?\n")
+if moon_land == "Y":
+    print("You exit the ship and are immediately abliterated by aliens")
+elif moon_land == "N":
+    print("You travel further into space searhing for aliens!")
+print('''
+ *********************************************************************************************
+                   |||
+                  +++++
+               __/ (_) \__
+          ____/_ ======= _\____
+ ________/ _ \(_)_______(_)/ _ \________
+<___+____ (_) | /   _   \ | (_) ____+___>
+  O O O  \___/ |   (_)   | \___/  O O O
+             \__\_______/__/
+
+***************************************************************************************
+''')
+  
