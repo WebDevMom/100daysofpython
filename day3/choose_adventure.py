9164c48f034c2d05309669b12e6ab5bf76b42e36
Carmen Smith <cheylaracarm77@gmail.com>
2023-03-27 19:34:39 -0500

    created choose_adventure.py and added image

---
+++ day3/choose_adventure.py
@@ -0,0 +1,20 @@
+print('''
+*******************************************************************************
+     o   o
+      )-(
+     (O O)
+      \=/
+     .-"-.
+    //\ /\\
+  _// / \ \\_
+ =./ {,-.} \.=
+     || ||
+     || ||    
+   __|| ||__  
+  `---" "---'
+*******************************************************************************
+''')
+print("Welcome to Space.")
+print("Your mission is to find the alien life.") 
+print("Your ship approaches a small moon!")
+input("Do you land on the moon and explore? Y or N")
