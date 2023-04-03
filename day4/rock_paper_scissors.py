6106472629344887eb489cae193adc5184bb28a8
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-02 21:28:16 -0500

    finished rock_paper_scissors

--- day4/rock_paper_scissors.py
+++ day4/rock_paper_scissors.py
@@ -47,16 +47,22 @@ else:
 if computer_select == select:   
     print("You Tied") 
 
-
-
 #computer 0 player 1- you win
 #computer 0 player 2- you lose
-
+if computer_select == 0 and select == 1:
+    print("You win")
+if computer_select == 0 and select == 2:
+    print("You lose")
 #computer 1 player 2 - you win
 #computer 1 player 0 - you lose
-
+if computer_select == 1 and select == 2:
+    print("You win")
+if computer_select == 1 and select == 0:
+    print("You lose")
 #computer 2 player 0 - you win
 #computer 2 player 1 - you lose
+if computer_select == 2 and select == 0:
+    print("You win")
+if computer_select == 2 and select == 1:
+    print("You lose")
 
-
-#Type 0 for Rock, 1 for Paper or 2 for Scissors
