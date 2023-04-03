c91dcc5be737a099269142a2f57951edfb6db750
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-02 21:18:09 -0500

    updated rock_paper_scissors

--- day4/rock_paper_scissors.py
+++ day4/rock_paper_scissors.py
@@ -1,5 +1,5 @@
 import random
-select = input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors.")
+select = int(input("What do you choose? Type 0 for Rock, 1 for Paper or 2 for Scissors."))
 
 rock = '''
     _______
@@ -27,12 +27,36 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-if select == "0":
+print("\nYou chose:")
+if select == 0:
     print(rock)
-elif select == "1":
+elif select == 1:
+    print(paper)
+else:
+     print(scissors)
+print("\nComputer chose:")
+computer_select = random.randint(0, 2)
+if computer_select == 0:
+    print(rock)
+elif computer_select == 1:
     print(paper)
 else:
      print(scissors)
 
-#no ideas
-computer_select = random.randint(0, 2)
+#decide if player won lost or tied
+if computer_select == select:   
+    print("You Tied") 
+
+
+
+#computer 0 player 1- you win
+#computer 0 player 2- you lose
+
+#computer 1 player 2 - you win
+#computer 1 player 0 - you lose
+
+#computer 2 player 0 - you win
+#computer 2 player 1 - you lose
+
+
+#Type 0 for Rock, 1 for Paper or 2 for Scissors
