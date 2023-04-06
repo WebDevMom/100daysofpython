06df748fc0a4d9c22b2f07cb0a7c847d9ca9abb8
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-05 21:09:53 -0500

    completed high score program

--- day5/high_score.py
+++ day5/high_score.py
@@ -2,3 +2,10 @@ student_scores = input("Input a list of student scores ").split()
 for n in range(0, len(student_scores)):
   student_scores[n] = int(student_scores[n])
 print(student_scores)
+
+high_score = 0
+for score in student_scores:
+  if score > high_score:
+    high_score = score
+  
+print(f"The highest score in the class is: {high_score}")
