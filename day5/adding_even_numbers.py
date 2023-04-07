de932ae1aee2d80551bb0dc6c95dbaa254ef1962
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-06 21:52:13 -0500

    completed adding_even_numbers

--- day5/adding_even_numbers.py
+++ day5/adding_even_numbers.py
@@ -1,5 +1,9 @@
 #You are going to write a program that calculates the sum of all the even numbers 
 #from 1 to 100. Thus, the first even number would be 2 and the last one is 100:1 
 #print statement in your console output. It should just print the final total and
-#not every step of the calculation
 #you will need to use the range() function in any of the solutions.
+
+total = 0
+for even_number in range(2, 101, 2):
+    total += even_number
+print(total)
