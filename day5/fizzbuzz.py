1e20f649e10eba323180e3f05580e3d9d9069ffc
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-08 00:32:42 -0500

    corrected error

--- day5/fizzbuzz.py
+++ day5/fizzbuzz.py
@@ -1,7 +1,7 @@
 for number in range(1, 101):
     if number % 3 == 0 and number % 5 == 0:
         print("FizzBuzz")
-    if number % 3 == 0:
+    elif number % 3 == 0:
         print("Fizz")
     elif number % 5 == 0:
         print("Buzz")
