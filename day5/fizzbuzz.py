aed719a466da839740c0402c203106b4397465dc
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-08 00:28:16 -0500

    completed fizzbuzz

--- day5/fizzbuzz.py
+++ day5/fizzbuzz.py
@@ -1,13 +1,10 @@
-#Write a program that automatically prints the solution to the FizzBuzz game.
+for number in range(1, 101):
+    if number % 3 == 0 and number % 5 == 0:
+        print("FizzBuzz")
+    if number % 3 == 0:
+        print("Fizz")
+    elif number % 5 == 0:
+        print("Buzz")
+    else:
+        print(number)
 
-#Program should print each number from 1 to 100 in turn.
-
-#When the number is divisible by 3 then instead of printing the number it should print "Fizz".
-
-#When the number is divisible by 5, then instead of printing the number it should print "Buzz".`
-
-#And if the number is divisible by both 3 and 5 e.g. 15 then instead of the number it should print "FizzBuzz"
-
-#Hint
-#Remember your answer should start from 1 and go up to and including 100.
-#2. Each number/text should be printed on a separate line.
