2e3f3019140abf05349d4db043b17e5fd4c1663e
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-11 18:25:40 -0500

    while lop practice

--- day5/for_loop_practice.py
+++ day5/for_loop_practice.py
@@ -1,4 +1,12 @@
-fruits = ["Apple", "Peach", "Pear"] 
-for x in fruits:
-    print(x)
-    print(x + " pie")
+#fruits = ["Apple", "Peach", "Pear"] 
+#for x in fruits:
+#    print(x)
+#    print(x + " pie")
+
+
+
+#while loops---only put true or false statements
+number_of_loops = 4
+while number_of_loops > 0:
+    print("it's time to party") 
+    number_of_loops -= 1  
