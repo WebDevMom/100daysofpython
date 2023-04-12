c5140438f67114058db355d5fd9a3e41ee23d4ce
Carmen Smith <cheylaracarm77@gmail.com>
2023-04-11 19:56:00 -0500

    updated practice

--- day5/for_loop_practice.py
+++ day5/for_loop_practice.py
@@ -6,7 +6,12 @@
 
 
 #while loops---only put true or false statements
-number_of_loops = 4
-while number_of_loops > 0:
-    print("it's time to party") 
-    number_of_loops -= 1  
+#number_of_loops = 4
+#while number_of_loops > 0:
+  #  print("it's time to party") 
+ #   number_of_loops -= 1  
+
+print(5)
+x = 5
+print(x + 1)
+
