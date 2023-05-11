5147bd6c0daa4e8b62d49935078a77f5ce1c13d4
Carmen Smith <cheylaracarm77@gmail.com>
2023-05-10 19:31:20 -0500

    created number double program

---
+++ multiply_two-numbers.py
@@ -0,0 +1,10 @@
+def main():
+    print("This program multiplies two numbers.")
+    # your code here
+    num1 = input("Enter first number: ")
+    num1 = int(num1)
+    num2 = input("Enter second number: ")
+    num2 = int(num2)
+    total = num1 * num2 
+    print(total)
+    
